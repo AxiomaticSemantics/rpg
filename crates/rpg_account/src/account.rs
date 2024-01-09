5715897d9b2fe7d26a5733619f2330f37ef53592
ebola <dev@axiomatic>
2024-01-09 09:32:12 -0500

    Network protocol additions

--- crates/rpg_account/src/account.rs
+++ crates/rpg_account/src/account.rs
@@ -2,13 +2,13 @@ use crate::character::{Character, CharacterInfo};
 
 use serde_derive::{Deserialize as De, Serialize as Ser};
 
-#[derive(Debug, Ser, De)]
+#[derive(Debug, Clone, Ser, De)]
 pub struct Account {
     pub info: AccountInfo,
     pub characters: Vec<Character>,
 }
 
-#[derive(Debug, Ser, De)]
+#[derive(Debug, Clone, PartialEq, Ser, De)]
 pub struct AccountInfo {
     pub name: String,
     pub character_info: Vec<CharacterInfo>,
