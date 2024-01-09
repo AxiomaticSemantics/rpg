4cbf0f86f790bc59d5717fb6e75a93f07f7d47c8
ebola <dev@axiomatic>
2024-01-09 02:40:46 -0500

    Additional work related to account storage.

--- crates/rpg_account/src/account.rs
+++ crates/rpg_account/src/account.rs
@@ -1,7 +1,11 @@
+use serde_derive::{Deserialize as De, Serialize as Ser};
+
+#[derive(Debug, Ser, De)]
 pub struct AccountCharacter {
     pub name: String,
 }
 
+#[derive(Debug, Ser, De)]
 pub struct Account {
     pub name: String,
     pub characters: Vec<AccountCharacter>,
--- crates/rpg_server/src/account.rs
+++ crates/rpg_server/src/account.rs
@@ -1,4 +1,4 @@
-use crate::server::{ClientType, NetworkContext};
+use crate::server::{ClientType, NetworkContext, NetworkParamsRO, NetworkParamsRW};
 
 use bevy::{
     ecs::{
@@ -18,18 +18,27 @@ use util::fs::{open_read, open_write};
 
 use std::{fs::Metadata, path::Path};
 
+// FIXME there should be different message types for admin and player variants
 pub(crate) fn receive_account_create(
-    mut commands: Commands,
     mut account_create_reader: EventReader<MessageEvent<CSCreateAccount>>,
-    mut context: ResMut<NetworkContext>,
+    net_params: NetworkParamsRO,
 ) {
     for event in account_create_reader.read() {
+        let client = net_params.context.clients.get(event.context()).unwrap();
+        if client.is_authenticated() && client.is_player() {
+            info!("already authenticated cliented attempted to create account {client:?}");
+            continue;
+        }
+
+        // Allow authenticated admins to create accounts
+        if client.is_authenticated_admin() {}
+
         let file_path = format!("save/server/accounts/{}", event.message().name);
         let path = Path::new(file_path.as_str());
         let file = open_read(path);
 
-        if let Ok(file) = file {
-            let fs_meta = file.metadata().unwrap();
+        if let Ok(_) = file {
+            info!("account already exists");
         } else {
             let Ok(file) = open_write(path) else {
                 info!("unable to open account file for writing");
@@ -45,17 +54,17 @@ pub(crate) fn receive_account_create(
 pub(crate) fn receive_account_load(
     mut commands: Commands,
     mut account_load_reader: EventReader<MessageEvent<CSLoadAccount>>,
-    mut context: ResMut<NetworkContext>,
+    net_params: NetworkParamsRO,
 ) {
     for event in account_load_reader.read() {
-        //
+        let client = net_params.context.clients.get(event.context()).unwrap();
     }
 }
 
 pub(crate) fn receive_character_create(
     mut commands: Commands,
     mut character_create_reader: EventReader<MessageEvent<CSCreateCharacter>>,
-    mut context: ResMut<NetworkContext>,
+    net_params: NetworkParamsRO,
 ) {
     for event in character_create_reader.read() {
         //
@@ -65,7 +74,7 @@ pub(crate) fn receive_character_create(
 pub(crate) fn receive_character_load(
     mut commands: Commands,
     mut character_load_reader: EventReader<MessageEvent<CSLoadCharacter>>,
-    mut context: ResMut<NetworkContext>,
+    net_params: NetworkParamsRO,
 ) {
     for event in character_load_reader.read() {
         //
@@ -75,11 +84,11 @@ pub(crate) fn receive_character_load(
 pub(crate) fn receive_connect_player(
     mut commands: Commands,
     mut connect_reader: EventReader<MessageEvent<CSConnectPlayer>>,
-    mut context: ResMut<NetworkContext>,
+    mut net_params: NetworkParamsRW,
 ) {
     for player in connect_reader.read() {
         let client_id = player.context();
-        let client = context.clients.get_mut(client_id).unwrap();
+        let client = net_params.context.clients.get_mut(client_id).unwrap();
         if client.client_type != ClientType::Unknown {
             info!(
                 "client type {:?} attempted to authorize as player while already authorized",
